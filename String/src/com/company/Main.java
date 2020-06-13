6bd01dde0c2aeb0caa0bc8991be2ed2f7964b314
unknown <Aleksandr>
2020-06-13 19:12:28 +0300

    First commit to temporary branch

---
+++ String/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public void functionWithVaragrs(int ... v){
+        for (int x:v) {
+            System.out.println(x);
+        }
+    }
+
+   public static void main(String[] args) {
+	// write your code here
+        String string="bla";
+        System.out.println(string);
+        string="f";
+        System.out.println(string);
+        Main main = new Main();
+        main.functionWithVaragrs(1,2,3);
+        main.functionWithVaragrs(1);
+        main.functionWithVaragrs(1,2,3,4);
+    }
+}
